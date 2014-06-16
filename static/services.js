d7e25f8858e066f702528f4c4900cd3404639b11
Alpere Fischl <aubnox@gmail.com>
2014-06-16 16:35:19 -0400

    added the promo service
    responsible for $http calls; keeping promo state within and without

---
+++ static/services.js
@@ -0,0 +1,65 @@
+var promoService = angular.module('SAATapp')
+    .service( 'PromoService', [ '$rootScope', '$http',
+        function( $rootScope, $http ) {
+            var promos = [];
+                        
+            this.syncPromos = function(){
+                console.log('syncPromos() called');
+                $http({ method: 'GET', url: '/promos'}) 
+                    .success( function(data, status, headers, config) {
+                        promos = data;
+                    })
+                    .error( function(data, status, headers, config) {
+                        alert('getPromoData failed, check server');
+                        console.error(data, status);
+                    })
+                ; // end http
+            };
+            
+            this.addPromo = function ( promo ) {
+                $http({ method: 'POST', url: '/newPromo', data: promo}) 
+                    .success( function(data, status, headers, config) {
+                        promoService.promos.push( promo );
+                        $rootScope.$broadcast( 'promos.update' );
+                    })
+                    .error( function(data, status, headers, config) {
+                        alert('addPromo failed, check server');
+                        console.error(data, status);
+                    })
+                ; // end http
+            };
+            
+            this.getPromos = function(){
+                return promos;
+            };
+            
+            this.getPromoById = function( id ) {
+                var promos = promos;
+                for( var i=0 ; i < promos.length ; i++ ){
+                    console.log(promos.pid, id);
+                    if ( promos.pid == id ) {
+                        return promos[i];
+                    }
+                }
+                return false;
+            };
+            
+            this.getPromoByIndex = function(index){
+                return promos[index];
+            };
+            
+            /*
+            
+            ,
+            editPromo: function: ( changedPromo ) {  
+                $http({ method: 'PUT', url: '/editPromo', data: changedPromo})
+                    .success( function(data, status, headers, config) {
+                        promoService.promos[changedPromo..push( promo );
+                        $rootScope.$broadcast( 'promos.update' );
+                    })
+            }
+              
+            */
+        }
+    ]);
+promoService.$inject = ['$rootScope', '$http'];
