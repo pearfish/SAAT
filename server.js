732f959e0e71a8c19894c968d60d3bfdcb7229ff
Alpere the Magnificent <aubnox@gmail.com>
2014-06-16 22:46:43 -0400

    copypaste server changes from home version

--- server.js
+++ server.js
@@ -36,23 +36,35 @@ var serverConfig = {
 };
 
 //xian skull has the best quotes 
-var xianQuotes = {
+var xianQuotes = [
+	"Don't put me in your pack. It's stuffy in there. And it smells funny.",
+	"Polish me! I'm dusty!",
+	"Check out my teeth! They're shiny!",
+	"Legolas! Boromir! Come quick! I ... Oh, wait. Never mind.",
+	"You know what the best thing about being a skull is? I don't either.",
+	"I have problems opening myself up to the unconditional love of others.",
+	"Does this bone structure make me look fat?",
+	"I hear that you can get a powerful spell in ... in ... Oh. I forgot.",
+	"You should buy some nice skull wax. I'm starting to look dull.",
+	"Let me go and I'll give you three wishes. Just kidding!",
+	"Aaaaah!!! Aaaaahhh!!!! There's a spider inside me! Get it out! Get it out! It tickles!",
+	"Check me out! I'm a talking skull!",
+	"Bet you've never met a talking skull before. I'm high in calcium!",
+	"Sometimes you have to kill to be kind.",
+	"Mmmmm. I sure could use a deli sandwich. Mmmmm.",
+	"I yearn for a gentle caress of a nice female skull.",
+	"I could use a hat. A nice hat. With feathers or something. Why don't you buy me a hat? If you were the skull and I was the dopey sword guy walking all over the place, I'd buy you a hat.",
+	"You ever long for the sweet embrace of death, held back from you for so, so terribly long? I ask only to make conversation."
+];
 
-	quotes: [
-		//TODO: POPULATE
-	]
-};
 
 console.log("establishing database connection...");
 connection.connect();
 
-console.log('connection established, doing query....');
 connection.query('select * from oc_programs ocp join oc_program_details deets where ocp.program_ID = deets.program_ID AND MERCHANT_ID =10000481 LIMIT '+serverConfig.QUERY_LIMIT+';', function(err, rows, fields) {
 	if (err) {
 		console.error('WARNING - query failed.  Oh no!  The presentation is ruined!');
 	} else {
-		console.log('query successful, creating fake db...');
-		
 		for( i=0 ; i < rows.length ; i++ ){
 			//map the desired columns to temp object
 			var tempPromo = {
@@ -71,24 +83,78 @@ connection.query('select * from oc_programs ocp join oc_program_details deets wh
 });
 
 connection.end();
-console.log('server connection murdered in its prime');
+console.log('server connection closed');
 
-app.post('/test', function(req, res, next){
-    res.status(200).send("peachy");
+
+/* NEW API */
+
+app.post('/newPromo', function(req, res){
+    //TODO
 });
 
-app.get('/promos', function(req, res, next){
-	console.log("/promos called");
+app.put('/editPromo', function(req, res){
+    //TODO
+    
+    
+});
+
+app.get('/getAllPromos', function(req, res){
+        console.log("/promos called");
 	
 	res.setHeader('Content-Type', 'application/json');
 	res.status(200).json(localPromos.promos);
+});
+
+app.get('/getOnePromo', function(req, res){
+	console.log("/promos called");
+	
+	//PLACEHOLDER THING, TODO: GRAB REAL REQUEST PARAM
+	var reqPid = 10000293; //req.param.pid;
+	
+	if ( localPromos.promos[reqPid] == undefined ) {
+		res.status(400).send('promo with id '+reqPid+'not found');
+	} else {
+		res.setHeader('Content-Type', 'application/json');
+		res.status(200).json(localPromos.promos[reqPid]);
+	}
+});
+
+app.get('/getPromoByMer', function(req, res){
+	console.log('/getPromosByMer called');
+	
+	//PLACEHOLDER THING, TODO: GRAB REAL REQUEST PARAM
+	var reqMid = 10000481; //req.param.mid;
+	
+	var result = localPromos.promos.filter(function( obj ) {
+		return obj.mid == reqMid;
+	});
+	
+	if ( result.length == 0 ) {
+		res.status(400).send('promos for merchant'+reqMid+'not found');
+	} else {
+		res.setHeader('Content-Type', 'application/json');
+		res.status(200).json(result);
+	}
+});
+
+app.get('/getQuote', function(req, res){
+	var quote = { quote: xianQuotes[Math.floor((Math.random() * xianQuotes.length) + 1)] };
+	res.status(200).json(quote);
+});
+
+/* END NEW API */
+
+
+/* OLD API */
+
+app.post('/test', function(req, res, next){
+    res.status(200).send("peachy");
 });
 
 app.get('/specificPromos/:mid', function(req, res, next){
 	console.log("/specificPromos called");
 	res.status(400).send('TODO');
 });
-
 
 app.post('/newPromo', function(req, res, next){
 	console.log('/newPromo added');
@@ -114,6 +180,8 @@ app.post('/newPromo', function(req, res, next){
 		res.status(200).send('promotion added');
 	}
 });
+
+/* END OLD API */
 
 app.listen(5678);
 console.log("Started server on port 5678");
