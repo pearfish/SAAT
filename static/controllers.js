a33dfaba309c167657eadd4129c3270ca3966ada
Alpere the Magnificent <aubnox@gmail.com>
2014-06-24 22:43:57 -0400

    basic set choosing logic

--- static/controllers.js
+++ static/controllers.js
@@ -40,6 +40,17 @@ angular.module('SAATapp')
 		}
 	}])
 	.controller('GearCtrl', function($scope){
+		$scope.selections = {
+			helmet: '',
+			shoulder: '',
+			chest: '',
+			pantaloons: '',
+			hands: '',
+			feets: ''
+		}
+		$scope.setGear = function(slot, set) {
+			$scope.selections[slot] = set;	
+		};
 		$scope.slots = [
 			//all given exotic stats
 			{
@@ -73,12 +84,122 @@ angular.module('SAATapp')
 				celestial: 15
 			},
 			{
-				name: 'foot decorations',
+				name: 'feets',
 				major: 34,
 				minor: 24,
 				celestial: 15
 			}
-		]
+		];
+		$scope.sets = [
+			{
+				name: 'Berserker',
+				major:  'power',
+				minor1: 'precision',
+				minor2: 'ferocity'
+			},
+			{
+				name: 'Zealot',
+				major:  'power',
+				minor1: 'precision',
+				minor2: 'healing power'
+			},
+			{
+				name: 'Soldier',
+				major:  'power',
+				minor1: 'toughness',
+				minor2: 'vitality'
+			},
+			{
+				name: 'Valkyrie',
+				major:  'power',
+				minor1: 'vitality',
+				minor2: 'ferocity'
+			},			
+			{
+				name: 'Assassin',
+				major:  'precision',
+				minor1: 'power',
+				minor2: 'ferocity'
+			},			
+			{
+				name: 'Rampager',
+				major:  'precision',
+				minor1: 'power',
+				minor2: 'condition damage'
+			},
+			{
+				name: 'Knight',
+				major:  'toughness',
+				minor1: 'power',
+				minor2: 'precision'
+			},
+			{
+				name: 'Cavalier',
+				major:  'toughness',
+				minor1: 'power',
+				minor2: 'ferocity'
+			},
+			{
+				name: 'Settler',
+				major:  'toughness',
+				minor1: 'condition damage',
+				minor2: 'healing power'
+			},
+			{
+				name: 'Giver',
+				major:  'toughness',
+				minor1: 'boon duration',
+				minor2: 'healing power'
+			},
+			{
+				name: 'Sentinel',
+				major:  'vitality',
+				minor1: 'power',
+				minor2: 'toughness'
+			},
+			{
+				name: 'Shaman',
+				major:  'vitality',
+				minor1: 'condition damage',
+				minor2: 'healing power'
+			},
+			{
+				name: 'Carrion',
+				major:  'condition damage',
+				minor1: 'power',
+				minor2: 'vitality'
+			},
+			{
+				name: 'Rabid',
+				major:  'condition damage',
+				minor1: 'precision',
+				minor2: 'toughness'
+			},
+			{
+				name: 'Dire',
+				major:  'condition damage',
+				minor1: 'toughness',
+				minor2: 'vitality'
+			},
+			{
+				name: 'Cleric',
+				major:  'healing power',
+				minor1: 'power',
+				minor2: 'toughness'
+			},
+			{
+				name: 'Magi',
+				major:  'healing power',
+				minor1: 'precision',
+				minor2: 'vitality'
+			},
+			{
+				name: 'Apothecary',
+				major:  'healing power',
+				minor1: 'toughness',
+				minor2: 'condition damage'
+			}
+		];
 			
 	})
 ; // end module
